f4d5809f3ec0067bc61c51903fceac040145419f
Sarah R. Vieira <sarahrdvieira@gmail.com>
2023-04-17 15:17:07 -0300

    Reorganize files with cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,9 @@
 #! /usr/bin/env node
 const chalk = require('chalk')
+const path = require('path')
+const mdLinks = require('../src/index.js')
+const filePath = path.join('src', 'example.md');
+
+mdLinks(filePath)
 
 console.log(chalk.blue('hiii'))
--- src/index.js
+++ src/index.js
@@ -1,33 +1,48 @@
 const fs = require('fs');
-const path = require('path')
 const http = require('http')
-const status = require('./status')
-const filePath = path.join('src', 'example.md');
-const ext = path.extname(filePath)
 
-  function getLinks(filePath) {
+  function mdLinks(filePath) {
     return fs.promises.readFile(filePath, 'utf8')
     .then(data => {
     const regex = /(?=\[(!\[.+?\]\(.+?\)|.+?)]\((https:\/\/[^\)]+)\))/gi
-    const links = [...data.matchAll(regex)].map((link =>({ text: link[1], URL: link[2], file: filePath})))
+    const areLinks = [...data.matchAll(regex)].map(link => {
 
-    links.forEach(link => {
-      console.log(link)
+      const linkInfo = { 
+        text: link[1], 
+        URL: link[2], 
+        file: filePath
+      }
+      
+      console.log(linkInfo)
     })
   })
   .catch(error => { throw error });
   }
 
-  function mdLinks(path) {
-    if (ext === '.md') {
-    return getLinks(path)
-    }
-    // status.getStatus(link)
+  function getStatus(response, error) {
+    fetch(link) 
+    .then(response => {
+      if (response.status === 200 || 201) {
+        console.log('link OK!')
+        console.log(response.status)
+      }
+    })
+    .catch(error => {
+      if (response.status === 400 || 404) {
+        console.log('link bad :(')
+      }
+    })
   }
+getStatus(link)
 
-mdLinks(filePath)
+//   function mdLinks(path) {
+//     if (ext === '.md') {
+//     return getLinks(path)
+//     }
+//     // status.getStatus(link)
+//   }
+
+// mdLinks(filePath)
 
 
-module.exports = () => {
-  // ... mdLinks() { ... }
-}
+module.exports = mdLinks
