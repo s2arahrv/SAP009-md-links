7b660cf58404ce970e751037aaf1a2283f233f8b
Sarah R. Vieira <sarahrdvieira@gmail.com>
2023-04-24 19:50:13 -0300

    eslint config

--- bin/cli.js
+++ bin/cli.js
@@ -1,49 +1,49 @@
 #! /usr/bin/env node
 const chalk = require('chalk');
 const path = require('path');
-const  { mdLinks, getStats, brokenStats, getStatus } = require('../src/index.js')
-const filePath = path.join('src', 'example.md') // process.argv[2]
-const options = process.argv.slice(2)
+const {
+  mdLinks, getStats, getStatus,
+} = require('../src/index');
 
-const validate = options[0] === '--validate' || options[1] === '--validate'
-const stats = options[0] === '--stats' || options[1] === '--stats'
+const filePath = path.join('src', 'example.md'); // process.argv[2]
+const options = process.argv.slice(2);
+
+const validate = options[0] === '--validate' || options[1] === '--validate';
+const stats = options[0] === '--stats' || options[1] === '--stats';
 
 if (options.length === 0) {
-mdLinks(filePath, { validate: false })
-.then(links => {
-    links.forEach(link => {
-          const output = chalk.blue(`${link.href} `) + 
-                         chalk.red(`${link.text} `) + 
-                         chalk.yellow(`${link.file}`)
-          console.log(output)
-      })
+  mdLinks(filePath, { validate: false })
+    .then((links) => {
+      links.forEach((link) => {
+        const output = chalk.blue(`${link.href} `)
+                         + chalk.red(`${link.text} `)
+                         + chalk.yellow(`${link.file}`);
+        console.log(output);
+      });
     })
-.catch(console.error)
+    .catch(console.error);
+} else if (validate) {
+  mdLinks(filePath, { validate: true })
+    .then((links) => {
+      links.forEach((link) => {
+        getStatus(link.href)
+          .then((result) => {
+            if (!stats) {
+              const output = chalk.green(`${result.ok} `)
+                               + chalk.green(`${result.status} `);
+              console.log(output);
+            }
+          });
+      });
+    });
 }
 
-else if (validate) {
-    mdLinks(filePath, { validate: true })
-    .then(links => {
-        links.forEach((link) => {
-            getStatus(link.href)
-            .then(result => {
-                if (!stats){
-                const output=  chalk.green(`${result.ok} `) +
-                               chalk.green(`${result.status} `)
-                console.log(output)
-                }
-            })
-            })
-            })
-        } 
-
-if(stats) {
-    mdLinks(filePath, {validate: false})
-    .then(links => {
-        console.log(getStats(links))
-    })
+if (stats) {
+  mdLinks(filePath, { validate: false })
+    .then((links) => {
+      console.log(getStats(links));
+    });
 }
-
 
 // mdLinks("./some/example.md")
 //   .then(links => {
--- src/index.js
+++ src/index.js
@@ -1,59 +1,59 @@
 const fs = require('fs');
-const { resolve } = require('path');
 
-  function fileExists(route) {
-    fs.existsSync(route)
-  }
+// function fileExists(route) {
+//   fs.existsSync(route);
+// }
 
-  function getLinks(filePath) {
-    return fs.promises.readFile(filePath, 'utf8')
-    .then(data => {
-    const regex = /(?=\[(!\[.+?\]\(.+?\)|.+?)]\((https:\/\/[^\)]+)\))/gi
-    const links = [...(data.matchAll(regex))]
-    const arrayLinks = links.map((link) => ({ 
-        text: link[1], 
-        href: link[2], 
-        file: filePath
-      }))    
-      return arrayLinks
-    })
-  }
+function getLinks(filePath) {
+  return fs.promises.readFile(filePath, 'utf8')
+    .then((data) => {
+      const regex = /(?=\[(!\[.+?\]\(.+?\)|.+?)]\((https:\/\/[^\)]+)\))/gi;
+      const links = [...(data.matchAll(regex))];
+      const arrayLinks = links.map((link) => ({
+        text: link[1],
+        href: link[2],
+        file: filePath,
+      }));
+      return arrayLinks;
+    });
+}
 
-  function getStatus(href) {
-      return fetch(href)
-    .then((response) => {
+function getStatus(href) {
+  return fetch(href)
+    .then((response) {
       if (response.ok) {
-      const result = { ok: 'ok', status: response.status }
-      return result
+        const result = { ok: 'ok', status: response.status };
+        return result;
       }
-      else if (!response.ok) {
-        const badResult = { ok: 'fail', status: response.status}
-        return badResult
+      if (!response.ok) {
+        const badResult = { ok: 'fail', status: response.status };
+        return badResult;
       }
     })
-    .catch(error => {
-      const errorType = { ok: 'fail', status: error.cause.code } 
-      return errorType
-    })     
-  }
+    .catch((error) => {
+      const errorType = { ok: 'fail', status: error.cause.code };
+      return errorType;
+    });
+}
 
-  function getStats(arrLinks) {
-    const totalLinks = arrLinks.length
-    const uniqueLinks = new Set(arrLinks.map(link => link.href)).size
-    const stats = `Total: ${totalLinks}\nUnique: ${uniqueLinks}`
-    return stats
-  }
+function getStats(arrLinks) {
+  const totalLinks = arrLinks.length;
+  const uniqueLinks = new Set(arrLinks.map((link) => link.href)).size;
+  const stats = `Total: ${totalLinks}\nUnique: ${uniqueLinks}`;
+  return stats;
+}
 
-  function brokenStats(arrLinks) {
-    const brokenLinks = arrLinks.filter(({ ok }) => ok === 'fail').length
-    const brokenStats = `Broken: ${brokenLinks}`
-    return brokenStats
-  }
+function brokenStats(arrLinks) {
+  const brokenLinks = arrLinks.filter(({ ok }) => ok === 'fail').length;
+  const stats = `Broken: ${brokenLinks}`;
+  return stats;
+}
 
-  const mdLinks = (path, options) => 
-    new Promise((res, rej) => {
-      const allLinks = getLinks(path)
-      res(allLinks)
-      })
-    
-module.exports = { mdLinks, getStatus, getStats, brokenStats }
+const mdLinks = (path, options) => new Promise((res, rej) => {
+  const allLinks = getLinks(path);
+  res(allLinks);
+});
+
+module.exports = {
+  mdLinks, getStatus, getStats, brokenStats,
+};
